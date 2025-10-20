ec26f6b1cb49c7985d253eb8bbb22f13ecbba7ab
Nick Kramer <2532885+nkramer@users.noreply.github.com>
2025-10-19 23:07:36 -0700

    Fix buttons on the Keyboard Shortcuts dialog

--- pictureviewer/shell/KeyboardShortcutsWindow.xaml.cs
+++ pictureviewer/shell/KeyboardShortcutsWindow.xaml.cs
@@ -12,10 +12,6 @@ namespace Pictureviewer.Shell {
             shortcutsItemsControl.ItemsSource = sections;
         }
 
-        private void closeButton_Click(object sender, RoutedEventArgs e) {
-            this.Close();
-        }
-
         private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
             if (e.ChangedButton == MouseButton.Left && e.OriginalSource == this) {
                 this.Close();
