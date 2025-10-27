32acc4aff531e2a3a0a9b50e7abffd19cbefc529
Nick Kramer <2532885+nkramer@users.noreply.github.com>
2025-10-26 21:25:57 -0700

    fixed

--- Folio.Tests/Book/AspectPreservingGridTests.cs
+++ Folio.Tests/Book/AspectPreservingGridTests.cs
@@ -4,16 +4,27 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Folio.Tests.Book
 {
     public class AspectPreservingGridTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public AspectPreservingGridTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public void ComputeSizes_ShouldHandleAllTemplatesWithoutThrowing()
         {
-            Exception testException = null;
+            var failures = new System.Collections.Generic.List<string>();
+            var successes = new System.Collections.Generic.List<string>();
+            Exception setupException = null;
             var thread = new Thread(() =>
             {
                 try
@@ -35,25 +46,68 @@ namespace Folio.Tests.Book
                     templateNames.Should().NotBeEmpty("there should be at least one template");
                     foreach (var templateName in templateNames)
                     {
-                        var bookModel = new BookModel();
-                        var pageModel = new PhotoPageModel(bookModel) { TemplateName = templateName };
-                        var pageView = new PhotoPageView { Page = pageModel };
-                        var testSize = new Size(1125, 875);
-                        var exception = Record.Exception(() => pageView.Measure(testSize));
-                        exception.Should().BeNull($"template '{templateName}' should not throw during layout");
+                        try
+                        {
+                            var bookModel = new BookModel();
+                            var pageModel = new PhotoPageModel(bookModel) { TemplateName = templateName };
+                            var pageView = new PhotoPageView { Page = pageModel };
+                            pageView.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                            pageView.Arrange(new Rect(0, 0, 1125, 875));
+                            var viewbox = pageView.FindName("templateContainer") as Viewbox;
+                            if (viewbox == null)
+                            {
+                                failures.Add($"{templateName}: no templateContainer");
+                                continue;
+                            }
+                            var grid = viewbox.Child as AspectPreservingGrid;
+                            if (grid != null)
+                            {
+                                var testSize = new Size(1125, 875);
+                                var exception = Record.Exception(() =>
+                                {
+                                    grid.Measure(testSize);
+                                    grid.Arrange(new Rect(testSize));
+                                });
+                                if (exception != null)
+                                {
+                                    failures.Add($"{templateName}: {exception.Message}");
+                                }
+                                else
+                                {
+                                    successes.Add(templateName);
+                                }
+                            }
+                            else
+                            {
+                                successes.Add($"{templateName} (non-APG)");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            failures.Add($"{templateName}: {ex.Message}");
+                        }
                     }
                 }
                 catch (Exception ex)
                 {
-                    testException = ex;
+                    setupException = ex;
                 }
             });
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
             thread.Join();
-            if (testException != null)
+            if (setupException != null)
             {
-                throw testException;
+                throw setupException;
+            }
+            if (failures.Count > 0)
+            {
+                var failureReport = $"Failed templates ({failures.Count}/{failures.Count + successes.Count}):\n" +
+                                  string.Join("\n", failures) +
+                                  $"\n\nSucceeded ({successes.Count}/{failures.Count + successes.Count}):\n" +
+                                  string.Join("\n", successes);
+                _output.WriteLine(failureReport);
+                Assert.Fail(failureReport);
             }
         }
     }
